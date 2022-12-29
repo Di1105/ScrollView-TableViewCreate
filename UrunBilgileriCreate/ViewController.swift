f1424f364d38f2af96d06e547703bb696d020cd4
Di1105 <delcioglu@gmail.com.com>
2022-12-29 11:45:09 +0300

    Initial Commit

---
+++ UrunBilgileriCreate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UrunBilgileriCreate
+//
+//  Created by Dilara Elçioğlu on 29.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
